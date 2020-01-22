fd6250fe0a8d6048c86a513ab433cebfbe4ba03d
yguan <yguan@example.com>
2020-01-22 23:40:05 +0800

    update

--- talk2_list.py
+++ talk2_list.py
@@ -1,1 +1,17 @@
-#
+# an ordered list of variables is a list.
+# A list is iconed by its squared bracket.
+c = [1, 2, 3]
+c
+# some function can give out a list.
+# a function has a name
+# input for a function is in an parentheses
+d = list(range(6))
+d
+# we can use for loop to go over the item in the list
+for i in c:
+    c
+# we can to apply some operation for items during the looping.
+for i in c:
+    c + 1
+# we can do the operation in a list.
+[c + 1 for i in c]
